4d22cc27cd8789d46d0e67b2b0e35cdccdbc22e5
Roman-tech-cmd <romakovtunak.bk@gmail.com>
2025-10-27 21:44:55 +0300

    refactor4

--- Assets/Scripts/GemsData.cs
+++ Assets/Scripts/GemsData.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewGem", menuName = "Gems/Gem Data")]
 public class GemsData : ScriptableObject
 {
-    public enum Rare { common, unusual, rare, epic, legendary };
-    public Rare rareItem;
+    public StaticItemCharacteristicClass.Rare rareItem;
     public string gemName;
     public string gemDiscription;
     public int addHealth;
